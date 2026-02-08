1fa3a33eabb36f17b758b44f232cffdcf099674b
lakshyx7 <lakshaydeepm25@gmail.com>
2026-02-08 12:56:33 +0530

    Feat: Define TypeScript interfaces for User and API response

---
+++ src/types/index.ts
@@ -0,0 +1,24 @@
+// src/types/index.ts
+
+export interface User {
+  id: string;      
+  firstName: string; 
+  lastName: string;  
+  email: string;
+  phone: string;
+  city: string;
+  country: string;
+  avatarLarge: string; 
+  avatarThumb: string; 
+  jobTitle: string;   
+  department: string;  
+}
+
+
+export interface ApiResponse {
+  results: any[];
+  info: {
+    page: number;
+    results: number;
+  };
+}
