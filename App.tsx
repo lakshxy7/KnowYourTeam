c83a55df06b27a22d75c1db3fd5f80eca9ddb7a6
lakshyx7 <lakshaydeepm25@gmail.com>
2026-02-08 13:03:08 +0530

    Refactor: Wrap App with redux provider and persistGate

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './src/redux/store';
 import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
-  
   return (
-    <SafeAreaProvider>
-      <AppNavigator />
-    </SafeAreaProvider>
+    <Provider store={store}>
+
+      <PersistGate loading={<ActivityIndicator size="large" />} persistor={persistor}>
+        <SafeAreaProvider>
+          <AppNavigator />
+        </SafeAreaProvider>
+      </PersistGate>
+    </Provider>
   );
 };
 
