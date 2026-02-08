2817315262a38c2622a72a531fab264bbbee876d
lakshyx7 <lakshaydeepm25@gmail.com>
2026-02-08 13:12:08 +0530

    Config: Add teamReducer to store and enable persistence

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,30 +1,31 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import directoryReducer from './slices/directorySlice';
 
-// 1. Config Persistence
+
+import directoryReducer from './slices/directorySlice';
+import teamReducer from './slices/teamSlice'; 
+
 const persistConfig = {
   key: 'root',
   storage: AsyncStorage,
-  whitelist: ['directory'], 
+  whitelist: ['directory', 'team'], 
 };
 
 const rootReducer = combineReducers({
   directory: directoryReducer,
+  team: teamReducer, 
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
-
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
+    getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
